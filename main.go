4f36f65027ad48137da8c64a9ba5471a3ab883fe
Devanand <dreddy@clearpathnet.com>
2015-06-29 10:54:05 -0700

    Migrating to Glide package manager. Removed the src/

--- main.go
+++ main.go
@@ -1,45 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"config"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	_ "logging"
-	"net/http"
-	"server"
+	// "github.com/shirou/gopsutil"
 )
 
 func main() {
-
-	k, _ := server.ServerMemory()
-
-	test := config.Main("interface")
-
-	fmt.Println(test)
-
-	mar, _ := json.Marshal(k)
-
-	buf := bytes.NewBuffer(mar)
-	enc := json.NewEncoder(buf)
-	err := enc.Encode(mar)
-
-	if err != nil {
-		panic(err)
-	}
-
-	// response, _ := http.Get("http://www.google.com")
-
-	send, _ := http.Post("https://demo7965648.mockable.io/serverdata", "application/json", buf)
-
-	body, _ := ioutil.ReadAll(send.Body)
-
-	raw := bytes.NewBuffer(body)
-
-	s := raw.String()
-
-	fmt.Println(s)
-	fmt.Println(enc)
-
+	fmt.Println("Hello Agent!")
 }
